4f3f85e8200262010c0c3005097acea6983ca8a0
root <root@dedivps-43767-2.dedicloud.co.uk>
2015-09-08 19:26:44 +0000

    Re-enabled bitcoin deposits and address generation + support redirect.

--- htdocs/help.php
+++ htdocs/help.php
@@ -14,5 +14,4 @@ $remote_url = preg_replace("/[^a-z0-9\/-]/", "",$_REQUEST['url']);
 if ($remote_url) {
 	$remote_url = '&redirect_to='.urlencode('https://1ex.freshdesk.com/'.$remote_url);
 }
-Link::redirect('https://1ex.freshdesk.com/login/sso?name='.urlencode(!empty($user_info['first_name']) ? $user_info['first_name'].' '.$user_info['last_name'] : $user_info['user']).'&email='.urlencode($user_info['email']).'&amp;timestamp='.(time()).'&hash='.hash_hmac('md5',($user_info['first_name'].' '.$user_info['last_name'].$user_info['email'].(time())),$CFG->helpdesk_key).$remote_url);
-
+Link::redirect('https://1ex.freshdesk.com/login/sso?name='.urlencode(!empty($user_info['first_name']) ? $user_info['first_name'].' '.$user_info['last_name'] : $user_info['user']).'&email='.urlencode($user_info['email']).'&amp;timestamp='.(time()).'&hash='.hash_hmac('md5',((!empty($user_info['first_name']) ? $user_info['first_name'].' '.$user_info['last_name'] : $user_info['user']).$user_info['email'].(time())),$CFG->helpdesk_key).$remote_url);
