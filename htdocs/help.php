e4d5d68f8ca62e3db328fb705ba1cacd0ea0bf08
mbassan <moises@organic.com.pa>
2016-11-09 00:09:25 +0000

    Help panel integrated.

--- htdocs/help.php
+++ htdocs/help.php
@@ -6,5 +6,14 @@ if (User::$awaiting_token)
 elseif (!User::isLoggedIn())
 	Link::redirect('login.php');
 
+API::add('User','getInfo',array($_SESSION['session_id']));
+$query = API::send();
+$user_info = $query['User']['getInfo']['results'][0];
+$remote_url = preg_replace("/[^a-z0-9\/-]/", "",$_REQUEST['url']);
 
-// redirect to your support panel of choice here
+if ($remote_url) {
+	$remote_url = '&redirect_to='.urlencode('https://exccy.freshdesk.com/'.$remote_url);
+}
+
+Link::redirect('https://exccy.freshdesk.com/login/sso?name='.urlencode(!empty($user_info['first_name']) ? $user_info['first_name'].' '.$user_info['last_name'] : $user_info['user']).'&email='.urlencode($user_info['email']).'&amp;timestamp='.(time()).'&hash='.hash_hmac('md5',((!empty($user_info['first_name']) ? $user_info['first_name'].' '.$user_info['last_name'] : $user_info['user']).$user_info['email'].(time())),$CFG->helpdesk_key).$remote_url);
+
